ab9a38549b1a2ecc51fa8e97e772846f805d2729
b45i <amalshajan2011@gmail.com>
2022-02-05 22:10:29 +0530

    get quate

--- src/components/Quote.jsx
+++ src/components/Quote.jsx
@@ -1,16 +1,35 @@
+import { useEffect, useState } from 'react';
+
 const Quote = () => {
+    const [quote, setQuote] = useState('');
+    const styles = {
+        backgroundImage: 'url(https://picsum.photos/1080/1920)',
+    };
+
+    const getData = async () => {
+        const response = await fetch('/api/quotes');
+        const data = await response.json();
+        setQuote(data.quote);
+    };
+
+    useEffect(() => {
+        getData();
+    }, []);
+
     return (
         <div className="quotes-container">
-            <div className="quote">
+            <div className="quote" style={styles}>
                 {/* <img
                     className="quote-image"
                     src="https://picsum.photos/1080/1920"
                 ></img> */}
-                <div className="quote-text">Hello</div>
+                <div className="quote-text">{quote}</div>
             </div>
             <div className="actions">
                 <button className="btn btn-blue">Download</button>
-                <button className="btn btn-orange">Next</button>
+                <button className="btn btn-orange" onClick={getData}>
+                    Next
+                </button>
             </div>
         </div>
     );
